e33985e34c9541f6bca802cc2271f079cec21b27
Karin OGawA <e185744@ie.u-ryukyu.ac.jp>
2018-10-31 12:02:31 +0900

    version3 ユニットテスト用の初期コード
    LivingThingクラスを作り、Hero,Enemyクラスに継承させた。

--- src/main/jp/ac/uryukyu/ie/e185744/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e185744/Hero.java
@@ -1,6 +1,6 @@
 package jp.ac.uryukyu.ie.e185744;
 
-public class Hero {
+public class Hero extends LivingThing{
     String name;
     int hitPoint;
     int attack;
@@ -13,35 +13,24 @@ public class Hero {
      * @param attack ヒーローの攻撃力
      */
     public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+//        this.name = name;
+//        hitPoint = maximumHP;
+//        this.attack = attack;
+//        dead = false;
+//        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        super(name,maximumHP,attack);
     }
 
     /**
      * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
      * @return boolean
      */
-    public boolean isDead(){
-        return  dead;
-    }
-
-    public String getName(){
-        return name;
-    }
 
     /**
      * Enemyへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param e 攻撃対象
      */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
-    }
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
@@ -49,10 +38,10 @@ public class Hero {
      * @param damage 受けたダメージ
      */
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        super.hitPoint -= damage;
+        if( super.hitPoint < 0 ) {
+            super.dead = true;
+            System.out.printf("%sは道半ばで力尽きてしまった。\n", super.name);
         }
     }
 }
